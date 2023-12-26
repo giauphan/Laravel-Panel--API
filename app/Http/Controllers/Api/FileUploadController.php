aea5ae177c7612e2ecb1049a1c8746b886d0c10c
giauphan <giauphan@users.noreply.github.com>
2023-12-26 07:59:27 +0000

    chore: fix code style

--- app/Http/Controllers/Api/FileUploadController.php
+++ app/Http/Controllers/Api/FileUploadController.php
@@ -174,7 +174,7 @@ class FileUploadController extends Controller
             if ($record->exists) {
                 return [
                     'status' => 429,
-                    'database' => $migration->database
+                    'database' => $migration->database,
                 ];
             }
             $record->fill([
